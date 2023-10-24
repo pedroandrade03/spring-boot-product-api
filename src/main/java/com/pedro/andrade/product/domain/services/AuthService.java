9513aff590ab34fafab26854f8e6ec719b3edc6d
Pedro Andrade <pedrogamerp@gmail.com>
2023-10-24 13:16:07 -0300

    feat: auth service

---
+++ src/main/java/com/pedro/andrade/product/domain/services/AuthService.java
@@ -0,0 +1,19 @@
+package com.pedro.andrade.product.domain.services;
+
+import com.pedro.andrade.product.infrastructure.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username);
+    }
+}
